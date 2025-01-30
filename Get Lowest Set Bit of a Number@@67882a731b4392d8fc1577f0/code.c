6ad5d1ef1946f18659d5a9adba89cc432a527720
prachi-657 <prachi0657.becse24@chitkara.edu.in>
2025-01-30 12:00:55 +0530

    Solution to Get Lowest Set Bit of a Number

---
+++ Get Lowest Set Bit of a Number@@67882a731b4392d8fc1577f0/code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int position=0;
+    scanf("%d", &a);
+    while ((a & 1)==0 && a!0){
+        a>>=1;
+        position++;
+    }
+    if (a==0){
+        printf("No set bit found");
+    }else{
+        printf("%d",position);
+    }
+    return 0;
+}
